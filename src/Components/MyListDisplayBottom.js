7a8dab3b272e0831cbaadea64a8862499c259e84
CodeWithYourHeart <65440123+CodeWithYourHeart@users.noreply.github.com>
2021-07-15 20:28:29 -0700

    added keys to  my list bottom

--- src/Components/MyListDisplayBottom.js
+++ src/Components/MyListDisplayBottom.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function MyListDisplayBottom(props) {
-  const mappedMyListBottom = props.highestState.mylist.map(ele => <h2>{ele.title}</h2>)
+  const mappedMyListBottom = props.highestState.mylist.map(ele => <h2 key ={ele.id+0}>{ele.title}</h2>)
   return (
    
     <div>
